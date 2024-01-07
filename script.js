98f585a2c907293f02e06adbf9a68cf4b5f89a01
Kaycke <kaycke@ymail.com>
2024-01-07 09:07:01 -0300

    Adicionado conteúdo
    conteúdo adicionado

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
-var lista_produtos = document.getElementById('lista_produtos');
-var botao_postar = document.getElementById('botao_postar');
-
 
 
 document.querySelector('button#botao_postar').addEventListener('click', function(e) {
     e.preventDefault;
 
-    const capa_jogo = document.getElementById('imagem_produto').value;
-    const nome_produto = document.getElementById('nome_produto').value;
-    const valor_produto = document.getElementById('valor_produto').value;
+    const novo_elemento = document.createElement('div');
+    novo_elemento.classList.add('produto');
 
-    document.querySelector('section#lista_produtos').innerHTML += `
-    <div class="produto">
-        <img class="imagem_jogo" src="${capa_jogo}" alt="Placeholder">
-        <h4>${nome_produto}</h4>
-        <p>R$${valor_produto}</p>
-        <button class="adicionar_ao_carrinho">Adicionar ao carrinho</button>
-    </div>
-    `;
+    const capa_jogo = document.getElementById('imagem_produto').value;
+    const nome_jogo = document.getElementById('nome_produto').value;
+    const valor_jogo = parseFloat(document.getElementById('valor_produto').value);
+
+    novo_elemento.innerHTML = `
+        <img class="imagem_jogo" src="${capa_jogo}">
+        <h4>${nome_jogo}</h4>
+        <p>R$${valor_jogo}</p>
+        <button class="adicionar_ao_carrinho">Adicionar ao carrinho</button>`;
+
+
+    document.getElementById('lista_produtos').insertBefore(novo_elemento, document.getElementById('criador_anuncios'));
+
+    document.getElementById('imagem_produto').value = '';
+    document.getElementById('nome_produto').value = '';
+    document.getElementById('valor_produto').value = '';
+
+
 })
